eec3c29659fc03a30434f6d9892885bbb64b04ef
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-05-08 23:25:10 +0100

    navigation bar

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = (props) => {
+    return(
+        <div className = "sidenav">
+            <a href="#section">Home</a>
+            <a href="#section">Our Properties</a>
+            <a href="#section">Agents</a>
+            <a href="#section">Blog</a>
+            <a href="#section">Contact Us</a>
+        </div>
+    
+    );
+};
+
+export default NavBar;
