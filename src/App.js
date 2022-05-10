e30762dd6214d79b672cc274ac95482fb72bff99
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-05-10 23:23:58 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar/navbar';
-import {BrowserRouter as Router} from 'react-router-dom';
-import Sidebar from './components/SideBar'
-import house from './components/images/image1.jpg'
-function App() {
-    return (
-      <Router>
-        <Navbar />
-        <Sidebar />
-      <div>
-        <img src ={house} alt="sample estate" class="center"/>
-      </div>
-      </Router>
+import { Home } from './pages/Home';
 
+function App() {
+  return (
+    <div className='Estately'>
+      <Home/>
+    </div>
   );
 }
 
